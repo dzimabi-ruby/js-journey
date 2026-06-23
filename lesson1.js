64b513d4daa91fd0e66ee4561321938c186a4b7f
dzimabi ruby <dzimabiruby@st.ug.edu.gh>
2026-06-23 01:08:28 +0000

    start JavaScript learning

---
+++ lesson1.js
@@ -0,0 +1,21 @@
+let name = "Rubbie";
+let age = 100;
+let school = "University of Ghana";
+let isStudent = true;
+let hall;
+let boyfriend = null
+
+
+console.log(name);
+console.log(age);
+console.log(school);
+console.log(isStudent);
+console.log(hall);
+console.log(boyfriend)
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof school);
+console.log(typeof isStudent)
+console.log(typeof hall)
+console.log(typeof boyfriend);
