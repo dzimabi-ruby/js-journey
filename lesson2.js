fce5153d485e09f6e26e286e08b253f3094d15aa
dzimabi ruby <dzimabiruby@st.ug.edu.gh>
2026-06-23 02:22:48 +0000

    operators and comparison practice

---
+++ lesson2.js
@@ -0,0 +1,14 @@
+let num1 = 25
+let num2 = 75
+
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 ** 2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 === num2)
+console.log(num1 != num2)
