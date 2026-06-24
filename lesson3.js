7549e4353ec993600ca99a3f0b86be2165dc073c
dzimabi ruby <dzimabiruby@st.ug.edu.gh>
2026-06-24 01:58:43 +0000

    practice with conditionals

---
+++ lesson3.js
@@ -0,0 +1,28 @@
+let age = 19;
+if (age >= 18) {
+    console.log("Qualified");}
+else {console.log ("you are a baby");}
+
+let score =43// try for 72,88,56
+if (score>=80) { console.log("Excellent");}
+else if (score >=70) {
+    console.log ("Good");
+}
+ else if (score >=60) { 
+    console.log ("Credit");}
+    else if (score>=50) { 
+        console.log("Pass");}
+        else { console.log("Fail");}
+        console.log("Try harder, Push harder, you are Possible!!!")
+
+    let grade=44 // try 67 86 90
+        if (grade>=80) { console.log("A");}
+else if (grade >=70) {
+    console.log ("B");
+}
+ else if (grade >=60) { 
+    console.log ("C");}
+    else if (grade>=50) { 
+        console.log("D");}
+        else { console.log("F");}
+        console.log("Try harder, Push harder, you are Possible!!!")
