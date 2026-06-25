233da5801eee57869eeef490d48b1b7033c7fb40
dzimabi ruby <dzimabiruby@st.ug.edu.gh>
2026-06-25 12:27:30 +0000

    practice with loops

---
+++ lesson4.js
@@ -0,0 +1,28 @@
+for (i=1; i<=20; i++) {
+    console.log(i);
+}
+
+for (i=20; i>=1; i--) {
+    console.log(i);
+}
+
+for (i=1; i<=20; i++)
+{ if (i % 2===0)
+    {console.log(i);
+}}
+
+for (i=3; i<=15; i++) {
+    if (i%3===0) {console.log (i)}
+}
+
+for (i=1; i<=3; i++) {
+    console.log("Rubbie", i)
+}
+
+for (i=1; i<=3; i++) {
+    console.log("Rubbie and Precious", i)
+}
+
+for (i=1; i<=5; i++) {
+    console.log("***");
+}
