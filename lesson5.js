8d72fffb6f3dc04d2f6ccb1dc8c01c4786e05a9c
dzimabi ruby <dzimabiruby@st.ug.edu.gh>
2026-06-28 11:33:11 +0000

    practice with functions

---
+++ lesson5.js
@@ -0,0 +1,36 @@
+ function greet(name) {
+    return "Hello " + name;
+ }
+ console.log (greet ("Ruby"));
+ console.log(greet("Precious"))
+
+
+ let course = "Javascript"
+ function message(course) {
+   return "I am learning " + course
+ }
+ console.log(message(course));
+
+ function sqaure(number) {
+   return (number * number)
+ }
+console.log(sqaure(4));
+console.log(sqaure(10));
+console.log(sqaure(33));
+
+function isEven(number) {
+   if (number % 2 ===0 ) {return "true"}
+   else { return "false"}
+}
+
+console.log(isEven(4));
+console.log (isEven(99));
+
+function isOdd (num) {
+   if (num % 2 ===1) {return "Odd"}
+   else {return ("Even")}
+}
+console.log(isOdd(4));
+console.log(isOdd(97));
+console.log(isOdd(45));
+
