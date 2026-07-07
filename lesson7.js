6b1463599735c91fd37537f63de596ddef775fda
dzimabi ruby <dzimabiruby@st.ug.edu.gh>
2026-07-07 00:34:30 +0000

    object practice

---
+++ lesson7.js
@@ -0,0 +1,19 @@
+let laptop ={
+    brand: "HP",
+ ram: 8,
+ storage: 512
+}
+console.log(laptop.brand);
+laptop.ram = 16
+console.log(laptop.ram)
+
+let student = {
+    name: "Rubbie",
+    age: 17,
+    course: "Computer Science" 
+
+}
+console.log (student.name)
+student.age = 18 
+student.level = 100
+console.log(student)
